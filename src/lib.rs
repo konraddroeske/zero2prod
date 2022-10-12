6640018947709bbac4b58d5edb9a17332118176c
Konrad Droeske <kdroeske@gmail.com>
2022-10-12 21:23:57 +0700

    added db script, split endpoints into routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,11 @@
+// pub mod configuration;
+// pub mod startup;
+pub mod routes;
+
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
+use routes::{health_check, subscribe};
 use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
