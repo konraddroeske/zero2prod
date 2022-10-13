4d2a756f1e3de6c6042fec99d68eb91a5c80f44a
Konrad Droeske <kdroeske@gmail.com>
2022-10-13 08:36:09 +0700

    added db connection, tests, updated github actions

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -19,7 +19,7 @@ then
 fi
 
 DB_USER="${POSTGRES_USER:=postgres}"
-DB_PASSWORD="${POSTGRES_PASSWORD:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-// pub mod configuration;
-// pub mod startup;
+pub mod configuration;
 pub mod routes;
+pub mod startup;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
